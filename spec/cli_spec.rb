35d7dd81659e9e41986eeb128e825b65b289d3bc
Nick Novitski <nicknovitski@gmail.com>
2011-12-26 23:56:18 -0800

    add tests for rating all media

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,27 +4,48 @@ describe "'oldness' command" do
   before :all do
     Dir.chdir("../bin")
   end
-  let(:command) {'./oldness'}
+
   context "followed by the 'rating' sub-command" do
-    let(:sub_command) {'rating'}
+
+    def cli_rating(string)
+      `./oldness rating #{string}`
+    end
     context "followed by a medium name and a date" do
       it "can rate a work of literature" do
-        `#{command} #{sub_command} literature 2001`.should == "*"
-        `./oldness rating literature 1889-4`.should == "***"
-        `./oldness rating literature 1889-4-26`.should == "**"
-        `./oldness rating literature -100`.should == "*****"
-        `./oldness rating literature -100-12-25`.should == "*****"
+        cli_rating('literature 2001').should == "*\n"
+        cli_rating('literature 1889-4').should == "***\n"
+        cli_rating('literature 1889-4-26').should == "**\n"
+        cli_rating('literature -100').should == "*****\n"
+        cli_rating('literature -100-12-25').should == "*****\n"
       end
       it "can rate a work of philosophy or religion" do
-        `#{command} #{sub_command} philosophy 1700`.should == '***'
-        `#{command} #{sub_command} religion 1911`.should == '**'
+        cli_rating('philosophy 1700').should == "***\n"
+        cli_rating('religion 1911').should == "**\n"
       end
-      it "knows the ranges for novels and books"
-      it "knows the ranges for films and movies"
-      it "knows the ranges for comics and manga"
-      it "knows the ranges for videogames"
-      it "knows the ranges for histories"
-      it "knows the ranges for cartoons, animation and anime"
+      it "can rate a novel or book" do
+        cli_rating('novel 1900').should == "****\n"
+        cli_rating('book 1900').should == "****\n"
+      end
+      it "can rate a film or movie" do
+        cli_rating('film 1900').should == "*****\n"
+        cli_rating("movie 1900").should == "*****\n"
+      end
+      it "can rate a comic or manga" do
+        cli_rating("comic 1970").should == "****\n"
+        cli_rating("manga 1970").should == "****\n"
+      end
+      it "can rate a videogame" do
+        cli_rating("videogame 2005-4-1").should == "***\n"
+      end
+      it "can rate a history" do
+        cli_rating("history 1922-4-1").should == "**\n"
+        #non-fiction?  (auto)biography?
+      end
+      it "can rate a cartoon, animation or anime" do
+        cli_rating("cartoon 2000").should == "****\n"
+        cli_rating("animation 2000").should == "****\n"
+        cli_rating("anime 2000").should == "****\n"
+      end
     end
     context "followed by a pair of dates" do
       it "understands the format YYYY"
