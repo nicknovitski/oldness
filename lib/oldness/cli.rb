4bf5f9f1a0e65d1e2625cf002b09c49bd4e345f9
Nick Novitski <nicknovitski@gmail.com>
2012-01-04 11:12:29 -0800

    rename most CLI commands, but add aliases

--- lib/oldness/cli.rb
+++ lib/oldness/cli.rb
@@ -4,25 +4,29 @@ module Oldness
 
   class CLI < Thor
 
-    desc "rating MEDIUM DATE", "give a star rating of oldness for a work in MEDIUM made on DATE"
-    def rating(medium, date)
+    desc "rate MEDIUM DATE", "give a star rating of oldness for a work in MEDIUM made on DATE"
+    def rate(medium, date)
       print "#{get_class(medium).rate(date, :formatted => true)}\n"
     end
+    map "rating" => "rate"
 
-    desc "range MEDIUM", "show what dates fall under what star ratings for a work in MEDIUM"
-    def range(medium)
+    desc "ranges MEDIUM", "show what dates fall under what star ratings for a work in MEDIUM"
+    def ranges(medium)
       print "#{get_class(medium).ranges(:formatted=>true)}\n"
     end
+    map "range" => "ranges"
 
     desc "first MEDIUM", "print the first work in MEDIUM"
     def first(medium)
       print "#{get_class(medium).first}\n"
     end
 
-    desc "mediums", "List what mediums can be used in other commands"
-    def mediums
+    desc "media", "List what mediums can be used in other commands"
+    def media
       print Oldness::media.collect(&:to_s).sort.inject("") { |l, c| l + c.sub("Oldness::", "")+"\n"}.downcase
     end
+    map "mediums" => "media"
+    map "list" => "media"
 
     no_tasks do
       def get_class(medium)
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -18,14 +18,14 @@ module Oldness
       it "prints the string formatting of the Work object stored under the first method of the appropriate Medium class"
     end
 
-    describe "mediums" do
+    describe "media" do
       it "lists all subclasses of Medium in the object space" do
         list = []
         ObjectSpace.each_object(Class) do |c|
           next unless c.superclass == Medium
           list << c
         end
-        `./oldness mediums`.should == list.collect(&:to_s).sort.inject("") { |l, c| l + c.downcase.sub("oldness::", "")+"\n" }
+        `./oldness media`.should == list.collect(&:to_s).sort.inject("") { |l, c| l + c.downcase.sub("oldness::", "")+"\n" }
       end
     end
   end
