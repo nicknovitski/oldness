34d7621f498fd55a079bd4ca2b7ffb48dd736eca
Nick Novitski <nicknovitski@gmail.com>
2012-01-05 23:39:41 -0800

    test thor actions without testing thor

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,21 +1,38 @@
 require 'spec_helper'
+require 'stringio'
 
 module Oldness
-  describe "oldness" do
-    before :all do
-      Dir.chdir("../bin")
+  describe CLI do
+    before :each do
+      @io = StringIO.new
+      $stdout = @io
+    end
+    let(:cli) {CLI.new}
+
+    def compare(method, medium, date=nil)
+      cli.invoke(method, [medium, date].compact)
+      args = [method, date]
+      args << {:formatted => true} unless method == :first
+      @io.string.should == eval(medium.capitalize).send(*args.compact).to_s + "\n"
     end
 
-    describe "rating" do
-      it "calls the rating method of the named Medium class with :formatted=>true"
+
+    describe "rate" do
+      it "calls the rating method of the named Medium class with :formatted=>true" do
+        compare(:rate, "film", "1999")
+      end
     end
 
     describe "range" do
-      it "calls the range method of the named Medium class with :formatted=>true"
+      it "calls the range method of the named Medium class with :formatted=>true" do
+        compare(:ranges,"film")
+      end
     end
 
     describe "first" do
-      it "prints the string formatting of the Work object stored under the first method of the appropriate Medium class"
+      it "calls the first method of named Medium class" do
+        compare(:first, "film")
+      end
     end
 
     describe "media" do
@@ -25,7 +42,9 @@ module Oldness
           next unless c.superclass == Medium
           list << c
         end
-        `./oldness media`.should == list.collect(&:to_s).sort.inject("") { |l, c| l + c.downcase.sub("oldness::", "")+"\n" }
+
+        cli.invoke(:media)
+        @io.string.should == list.collect(&:to_s).sort.inject("") { |l, c| l + c.downcase.sub("oldness::", "")+"\n" }
       end
     end
   end
